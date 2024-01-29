9d563dbac85ad257425caa74c5d8a57c61e6e4a3
clendinning1 <149982688+clendinning1@users.noreply.github.com>
2024-01-29 11:31:53 -0600

    wrote to-do list in js file

---
+++ assets/js/script.js
@@ -0,0 +1,24 @@
+// TO DO
+// 1. WHEN I search for a city, THEN
+//      a. I am presented with current and future conditions for that city
+//      b. that city is added to the search history
+// 2. WHEN I view current weather conditions for that city THEN
+//      a. I am presented with the city name,
+//      b. the date,
+//      c. an icon representation of weather conditions,
+//      d. the temperature,
+//      e. the humidity,
+//      f. the wind speed
+// 3. WHEN I view future weather conditions for that city THEN
+//      a. I am presented with a 5-day forecast
+//      b. that displays the date,
+//      c. an icon representation of weather conditions,
+//      d. the temperature,
+//      e. the wind speed,
+//      f. the humidity
+// 4. WHEN I click on a city in the search history, THEN
+//      a. I am again presented with current and future conditions for that city
+
+
+
+// code
