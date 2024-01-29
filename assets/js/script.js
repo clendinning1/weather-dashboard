87abbb4250ad9f3adfab275a61d270ca3689343d
clendinning1 <149982688+clendinning1@users.noreply.github.com>
2024-01-29 11:41:16 -0600

    added requirements hint to js todo

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,9 @@
 // TO DO
+//
 // 1. WHEN I search for a city, THEN
 //      a. I am presented with current and future conditions for that city
 //      b. that city is added to the search history
+//
 // 2. WHEN I view current weather conditions for that city THEN
 //      a. I am presented with the city name,
 //      b. the date,
@@ -9,6 +11,7 @@
 //      d. the temperature,
 //      e. the humidity,
 //      f. the wind speed
+//
 // 3. WHEN I view future weather conditions for that city THEN
 //      a. I am presented with a 5-day forecast
 //      b. that displays the date,
@@ -16,9 +19,13 @@
 //      d. the temperature,
 //      e. the wind speed,
 //      f. the humidity
+//
 // 4. WHEN I click on a city in the search history, THEN
 //      a. I am again presented with current and future conditions for that city
-
+//
+// 5. Hint: Using the 5 Day Weather Forecast API, you'll notice that you will need
+//    to pass in coordinates instead of just a city name. Using the OpenWeatherMap
+//    APIs, how could we retrieve geographical coordinates given a city name?
 
 
 // code
