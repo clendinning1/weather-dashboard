ed411552dfc39c5643333d118f6ced6663349b67
clendinning1 <149982688+clendinning1@users.noreply.github.com>
2024-02-04 20:28:38 -0600

    rmved finished js todos

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,22 +4,6 @@
 //      a. I am presented with current and future conditions for that city
 //      b. that city is added to the search history
 //
-// 2. WHEN I view current weather conditions for that city THEN
-//      a. I am presented with the city name,
-//      b. the date,
-//      c. an icon representation of weather conditions,
-//      d. the temperature,
-//      e. the humidity,
-//      f. the wind speed
-//
-// 3. WHEN I view future weather conditions for that city THEN
-//      a. I am presented with a 5-day forecast
-//      b. that displays the date,
-//      c. an icon representation of weather conditions,
-//      d. the temperature,
-//      e. the wind speed,
-//      f. the humidity
-//
 // 4. WHEN I click on a city in the search history, THEN
 //      a. I am again presented with current and future conditions for that city
 //
@@ -92,7 +76,7 @@ async function pullForecast() {
     // fetch the info
     const response = await fetch(forecastURL);
     const forecast = await response.json();
-    
+
 
     for (let i = 3; i < 36; i++) {
         if (i == 3 || i == 11 || i == 19 || i == 27 || i == 35) {
