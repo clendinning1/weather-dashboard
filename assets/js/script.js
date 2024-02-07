5577f68c3066b23f0e9aa084a8d2fe9d976b5c9a
clendinning1 <149982688+clendinning1@users.noreply.github.com>
2024-02-06 20:51:39 -0600

    having trouble testing because pullAllWeather is running before pullCityInfo, presumably because the call answers faster in CurrentWeather than CityInfo? how do i fix this?

--- assets/js/script.js
+++ assets/js/script.js
@@ -24,134 +24,154 @@ var APIKey = "fc7efc392867b6b77f021be757a566cf";
 var cityInput = "austin"
 var geocodeURL = "http://api.openweathermap.org/geo/1.0/direct?q=" + cityInput + "&limit=1&appid=" + APIKey
 
-var cityLat = "30.26715000"
-var cityLon = "97.74306000"
-var cityCoords = "?lat=" + cityLat + "&lon=" + cityLon; // take in user input for city
+var cityLat = 0;
+var cityLon = 0;
+var cityStatus = cityLat + cityLon; // to test if pullAllWeather can run
 
-// URLs for weather and five day forecast
-var weatherURL = "https://api.openweathermap.org/data/2.5/weather" + cityCoords + "&appid=" + APIKey + "&units=imperial";
-var forecastURL = "https://api.openweathermap.org/data/2.5/forecast" + cityCoords + "&appid=" + APIKey + "&units=imperial";
+
+
+
+
+// recieve user input of city
+// then feed user input into pull city info
+// run city info then feed it into All Weather
+// then run All Weather to pull and display it
+
+
 
 async function pullCityInfo() {
     // fetch the info
     const cityResponse = await fetch(geocodeURL);
     const cityInfo = await cityResponse.json();
-    
-    var cityLatReal = cityInfo["0"]["lat"];
-    var cityLonReal = cityInfo["0"]["lon"];
-    console.log(cityLatReal + " + " + cityLonReal);
+
+    cityLat = cityInfo["0"]["lat"];
+    cityLon = cityInfo["0"]["lon"];
+
+
 }
 
 pullCityInfo();
 
 
-// CURRENT WEATHER
-async function pullCurrentWeather() {
-    // fetch the info
-    const weathResponse = await fetch(weatherURL);
-    const weath = await weathResponse.json();
-    console.log(weath);
-
-    // grab the city name
-    var currCity = weath["name"];
-    var currCityPrint = document.getElementById("currentcity");
-    currCityPrint.innerText = currCity + " ("
-
-    // grab the current date 
-    var dt = weath["dt"];
-    var day = new Date(dt * 1000);
-    // print it on the screen
-    var dtPrint = document.getElementById("currentdate");
-    dtPrint.innerText = (day.toDateString()) + ")";
+var cityCoords = "?lat=" + cityLat + "&lon=" + cityLon;
+// URLs for weather and five day forecast
+var weatherURL = "https://api.openweathermap.org/data/2.5/weather" + cityCoords + "&appid=" + APIKey + "&units=imperial";
+var forecastURL = "https://api.openweathermap.org/data/2.5/forecast" + cityCoords + "&appid=" + APIKey + "&units=imperial";
 
 
-    // grab the icon for current conditions
-    var iconID = weath["weather"]["0"]["icon"];
-    var iconURL = "https://openweathermap.org/img/wn/" + iconID + ".png"
-    // print it
-    var iconPrint = document.getElementById("currenticon");
-    var icon = document.createElement("img");
-    icon.src = iconURL;
-    iconPrint.appendChild(icon);
+async function pullAllWeather() {
+    // CURRENT WEATHER
+    async function pullCurrentWeather() {
+        // fetch the info
+        const weathResponse = await fetch(weatherURL);
+        const weath = await weathResponse.json();
+        console.log(weath);
+
+        // grab the city name
+        var currCity = weath["name"];
+        var currCityPrint = document.getElementById("currentcity");
+        currCityPrint.innerText = currCity + " ("
+
+        // grab the current date 
+        var dt = weath["dt"];
+        var day = new Date(dt * 1000);
+        // print it on the screen
+        var dtPrint = document.getElementById("currentdate");
+        dtPrint.innerText = (day.toDateString()) + ")";
 
 
-    // grab the current temp 
-    var temp = weath["main"]["temp"];
-    // print it
-    var tempPrint = document.getElementById("currenttemp");
-    tempPrint.innerText = "Temp: " + temp + " \u00B0F";
+        // grab the icon for current conditions
+        var iconID = weath["weather"]["0"]["icon"];
+        var iconURL = "https://openweathermap.org/img/wn/" + iconID + ".png"
+        // print it
+        var iconPrint = document.getElementById("currenticon");
+        var icon = document.createElement("img");
+        icon.src = iconURL;
+        iconPrint.appendChild(icon);
 
 
-    // grab the current wind speed
-    var wind = weath["wind"]["speed"];
-    // print it
-    var windPrint = document.getElementById("currentwind");
-    windPrint.innerText = "Wind: " + wind + " MPH";
+        // grab the current temp 
+        var temp = weath["main"]["temp"];
+        // print it
+        var tempPrint = document.getElementById("currenttemp");
+        tempPrint.innerText = "Temp: " + temp + " \u00B0F";
 
 
-    // grab the current humidity
-    var humid = weath["main"]["humidity"];
-    // print it
-    var humidPrint = document.getElementById("currenthumid");
-    humidPrint.innerText = "Humidity: " + humid + "%";
-}
-
-// 5-DAY FORECAST
-async function pullForecast() {
-    // fetch the info
-    const foreResponse = await fetch(forecastURL);
-    const forecast = await foreResponse.json();
-    console.log(forecast);
-
-    for (let i = 3; i < 36; i++) {
-        if (i == 3 || i == 11 || i == 19 || i == 27 || i == 35) {
-            // only applies if the iteration is one of those numbers.
-            // those 5 numbers are noon for each upcoming day in the forecast.
-
-            // grab dates
-            var dtF = forecast["list"][[i]]["dt"];
-            var dayF = new Date(dtF * 1000);
-            // print it
-            var dateEl = "date" + [i]
-            var dtFPrint = document.getElementById(dateEl);
-            dtFPrint.innerText = (dayF.toDateString());
-
-            // grab the icons
-            var iconIDF = forecast["list"][[i]]["weather"]["0"]["icon"];
-            var iconURLF = "https://openweathermap.org/img/wn/" + iconIDF + ".png"
-            // print them
-            var iconDiv = "day" + [i] + "icon";
-            var iconPrintF = document.getElementById(iconDiv);
-            var iconF = document.createElement("img");
-            iconF.src = iconURLF;
-            iconPrintF.appendChild(iconF);
+        // grab the current wind speed
+        var wind = weath["wind"]["speed"];
+        // print it
+        var windPrint = document.getElementById("currentwind");
+        windPrint.innerText = "Wind: " + wind + " MPH";
 
 
-            // grab the temps
-            var tempF = forecast["list"][[i]]["main"]["temp"];
-            // print them
-            var tempEl = "d" + [i] + "temp";
-            var tempFPrint = document.getElementById(tempEl);
-            tempFPrint.innerText = "Temp: " + tempF + " \u00B0F";
+        // grab the current humidity
+        var humid = weath["main"]["humidity"];
+        // print it
+        var humidPrint = document.getElementById("currenthumid");
+        humidPrint.innerText = "Humidity: " + humid + "%";
+    }
+
+    // 5-DAY FORECAST
+    async function pullForecast() {
+        // fetch the info
+        const foreResponse = await fetch(forecastURL);
+        const forecast = await foreResponse.json();
+        console.log(forecast);
+
+        for (let i = 3; i < 36; i++) {
+            if (i == 3 || i == 11 || i == 19 || i == 27 || i == 35) {
+                // only applies if the iteration is one of those numbers.
+                // those 5 numbers are noon for each upcoming day in the forecast.
+
+                // grab dates
+                var dtF = forecast["list"][[i]]["dt"];
+                var dayF = new Date(dtF * 1000);
+                // print it
+                var dateEl = "date" + [i]
+                var dtFPrint = document.getElementById(dateEl);
+                dtFPrint.innerText = (dayF.toDateString());
+
+                // grab the icons
+                var iconIDF = forecast["list"][[i]]["weather"]["0"]["icon"];
+                var iconURLF = "https://openweathermap.org/img/wn/" + iconIDF + ".png"
+                // print them
+                var iconDiv = "day" + [i] + "icon";
+                var iconPrintF = document.getElementById(iconDiv);
+                var iconF = document.createElement("img");
+                iconF.src = iconURLF;
+                iconPrintF.appendChild(iconF);
 
 
-            // grab the wind speeds
-            var windF = forecast["list"][[i]]["wind"]["speed"];
-            // print them
-            var windEl = "d" + [i] + "wind";
-            var windFPrint = document.getElementById(windEl);
-            windFPrint.innerText = "Wind: " + windF + " MPH";
+                // grab the temps
+                var tempF = forecast["list"][[i]]["main"]["temp"];
+                // print them
+                var tempEl = "d" + [i] + "temp";
+                var tempFPrint = document.getElementById(tempEl);
+                tempFPrint.innerText = "Temp: " + tempF + " \u00B0F";
 
 
-            // grab the humids
-            var humidF = forecast["list"][[i]]["main"]["humidity"];
-            // print them
-            var humidEl = "d" + [i] + "humid"
-            var humidFPrint = document.getElementById(humidEl);
-            humidFPrint.innerText = "Humidity: " + humidF + "%";
+                // grab the wind speeds
+                var windF = forecast["list"][[i]]["wind"]["speed"];
+                // print them
+                var windEl = "d" + [i] + "wind";
+                var windFPrint = document.getElementById(windEl);
+                windFPrint.innerText = "Wind: " + windF + " MPH";
+
+
+                // grab the humids
+                var humidF = forecast["list"][[i]]["main"]["humidity"];
+                // print them
+                var humidEl = "d" + [i] + "humid"
+                var humidFPrint = document.getElementById(humidEl);
+                humidFPrint.innerText = "Humidity: " + humidF + "%";
+            }
         }
     }
+
+    pullCurrentWeather();
+    pullForecast();
 }
 
-pullCurrentWeather();
-pullForecast();
+pullAllWeather();
+
+
